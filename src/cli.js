31f10d601879a38c563ab4dfa3acf9ad632667a8
crumblefive <therustam94@gmail.com>
2024-04-10 22:06:51 +0400

    update all

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,6 @@
 import readlineSync from 'readline-sync';
-console.log("Welcome to the Brain games!")
+
+console.log('Welcome to the Brain games!');
 const name = readlineSync.question('May I have your name?: ');
-console.log('Hello, '+ name + '!');
+console.log(`Hello,  ${name}!`);
 export default name;
