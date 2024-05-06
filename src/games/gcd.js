320fdd4aafe9d6eece8d9efce21f503d3d23c407
crumblefive <therustam94@gmail.com>
2024-05-06 20:50:48 +0400

    Update cli.js

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,21 +1,20 @@
-import { getRandomNumber, gameLogic } from '../index.js'
+import { getRandomNumber, gameLogic } from '../index.js';
 
 const gameRules = 'Find the greatest common divisor of given numbers.';
 
 const nod = (numb1, numb2) => {
-  if (numb2 === 0 ) {
+  if (numb2 === 0 ){
     return numb1;
   }
-  return nod (numb2, numb1 % numb2);
+  return nod(numb2, numb1 % numb2);
 };
 const generateRoundData = () => {
-    const num1 = getRandomNumber(1,100);
-    const num2 = getRandomNumber(1,100);
+  const num1 = getRandomNumber(1, 100);
+  const num2 = getRandomNumber(1, 100);
 
-    const question = `${num1} ${num2}`;
-    const correctAnswer = `${nod(num1, num2)}`;
-    return [question, correctAnswer];
+  const question = `${num1} ${num2}`;
+  const correctAnswer = `${nod(num1, num2)}`;
+  return [question, correctAnswer];
 };
 const nodGame = () => gameLogic(gameRules, generateRoundData);
-
 export default nodGame;
